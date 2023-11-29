ec358d76027a9609b7f5cdc4bab7f66d4087916d
aooooooo1 <pdqovvarxx@naver.com>
2023-11-30 08:04:00 +0900

    a

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql');
 const db= mysql.createPool({
-    host: 'localhost',
-    user : 'user_codingrecipe',
-    password: '1234',
-    database: 'db_codingrecipe',
+    // host: 'localhost',
+    // user : 'user_codingrecipe',
+    // password: '1234',
+    // database: 'db_codingrecipe',
+    host : 'ro2padgkirvcf55m.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user : 'hbh6ot75h89gjt21',
+    password : 'xvw2tvi8dv7294fx',
+    database : 'd9phcv15p2949b6j',
     port: '3306'
 })
 
