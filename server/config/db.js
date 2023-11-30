3476685f9b4493451ac0dd9f84a3349e1feee390
aooooooo1 <pdqovvarxx@naver.com>
2023-11-30 17:55:46 +0900

    a

--- server/config/db.js
+++ server/config/db.js
@@ -4,10 +4,10 @@ const db= mysql.createPool({
     // user : 'user_codingrecipe',
     // password: '1234',
     // database: 'db_codingrecipe',
-    host : 'ro2padgkirvcf55m.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user : 'l5a997w0zukh1vh9',
-    password : 't7za34mzqf1o98u3',
-    database : 'se1d9i0p64surr02',
+    host : 'j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user : 'eqmnewvsjo8szo21',
+    password : 'iu3er83b71e5ilyo',
+    database : 'jtpmgsbag6g69tg6',
     port: '3306'
 })
 
