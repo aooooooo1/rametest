4765ae0e4810c74c4e1ecee1030fe2de85a7f62a
aooooooo1 <pdqovvarxx@naver.com>
2023-11-30 17:47:07 +0900

    a

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {valiUser} = require('./middleware/auth');
+const path = require('path');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
@@ -14,6 +15,7 @@ app.use(
     })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/build")));
 const bcrypt = require('bcrypt');
 const PORT = process.env.PORT || 4000;
 const db = require('./config/db');
